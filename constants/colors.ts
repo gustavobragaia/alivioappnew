b195ee37b94d2448b6a16ccbc3c7360211ddd602
Gustavo Bragaia <52480533+gustavobragaia@users.noreply.github.com>
2025-06-17 18:08:32 -0300

    feat: colors file

---
+++ constants/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+    background: '#f8f9fb',
+    white: '#fff',
+    logo: {
+        background: '#09454f',
+        backgroundOverlay: '#04343d'
+    },
+    text: {
+        titleDarkGreen: '#001218',
+        
+    }
+}
+
+export default colors
